6c7ae2bb677be5a53185c46f6c9a3cf75828f02e
Evan Sackmann <evan.sackmann@hotmail.com>
2022-05-27 10:51:22 -0700

    Final

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -1,14 +1,14 @@
 function RulesModal() {
-	return (
-		<div className="rules">
-			<h1>RULES</h1>
-			<img
-				src="../images/image-rules.svg"
-				alt="Paper > Rock > Scissors > Paper"
-			/>
-			<button className="modal-close">X</button>
-		</div>
-	)
+  return (
+    <div className="rules">
+      <h1>RULES</h1>
+      <img
+        src="../images/image-rules.svg"
+        alt="Paper > Rock > Scissors > Paper"
+      />
+      <button className="modal-close">X</button>
+    </div>
+  );
 }
 
-export default RulesModal
+export default RulesModal;
